82f8a9bd806b48a9c897893754e17a0b24368997
Maurice Drew <drewm1987@gmail.com>
2025-03-13 09:18:28 -0500

    updated hw

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const freelancers = [
     { name: "Alice", occupation: "Writer", price: 30 },
     { name: "Bob", occupation: "Teacher", price: 50 },
-    { name: "Carol", occupation: "Programmer", price: 70 },
+    
   ];
   
 function init() {
@@ -14,9 +14,7 @@ h1.innerHTML = "freelancers forum"
 console.log(h1)
 body.append(h1);
 
-const h2 = document.createElement("h2")
-h2.innerHTML = "Available Freelancers"
-body.append(h2);
+
 
 function averagePrice () {
   const totalPrice = freelancers.reduce((total, person) => total+person.price,0)
@@ -31,8 +29,12 @@ function updatedAveragePrice() {
 
 const h3 = document.createElement("h3")
 h3.id = "averagePrice"
-h3.innerHTML = `Average Price: ${averagePrice()}`
+h3.innerHTML = `The Average starting Price: ${averagePrice()}`
 body.append(h3);
+
+const h2 = document.createElement("h2")
+h2.innerHTML = "Available Freelancers"
+body.append(h2);
 
 const freelancersContainer = document.createElement("table")
 freelancersContainer.id = "freelancerContainer"
@@ -49,13 +51,19 @@ function renderFreelancers () {
   })
   updatedAveragePrice()
 }
+function addCarol () {
+  const carol = { name: "Carol", occupation: "Programmer", price: 70 };
+  freelancers.push(carol)
+  renderFreelancers()
+}
+function addFreelancers (){
+  const randomIndex = Math.floor(Math.random() * newFreelancers.length)
+  const newfreelancer = newFreelancers[randomIndex]
+  freelancers.push(newFreelancer)
+  renderFreelancers()
+}
 
 
-// const freelancersContainer = document.createElement("div")
-// freelancersContainer.id = "freelancersContainer"
-// // freelancersContainer.classList.add()
-// freelancersContainer.classList = "freelancers"
-// console.log(freelancersContainer.classList)
-// root.append(freelancersContainer)
+
   }
   init()
